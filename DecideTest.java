a7e7edcf6d84f44131cd631c6c7260a1ac729597
rachedkko <rached112004@gmail.com>
2024-01-25 10:40:22 +0100

    Issue#1 : create test case file feat : creating a prototype test file

---
+++ DecideTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DecideTest {
+    @Test 
+    public void testDecide(){
+        //TODO
+    }
+    @Test
+    public void testFUVCreator(){
+        //TODO
+    }
+    @Test
+    public void testPUMCreator(){
+        //TODO
+    }
+    @Test
+    public void testCMVCreator(){
+        //TODO
+    }
+}
