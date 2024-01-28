d4018690e1f2895f499a22488991e1564efe6d9d
Alexander Huynh <alexander_huynh@icloud.com>
2024-01-28 22:58:45 +0100

    issue #16 implement cmv condition 14
    feat : implemented cmv condition 14 along with its unit tests

--- Decide.java
+++ Decide.java
@@ -364,8 +364,42 @@ public class Decide{
         return false;
     }
 
-     boolean CMV14(){
-        //TODO
+     boolean CMV14(double area1, double area2, int epts, int fpts){
+        if(NUMPOINTS < 5) return false;
+
+        double x1;
+        double y1;
+
+        double x2;
+        double y2;
+
+        double x3;
+        double y3;
+
+        boolean area1Condition = false;
+        boolean area2Condition = false;
+
+        for(int i = 0; i < NUMPOINTS - (epts + fpts + 2); i++) {
+            x1 = x[i];
+            y1 = y[i];
+
+            x2 = x[i + epts + 1];
+            y2 = y[i + epts + 1];
+
+            x3 = x[i + epts + fpts + 2];
+            y3 = y[i + epts + fpts + 2];
+
+            double triangleArea = triangleArea(x1, y1, x2, y2, x3, y3);
+
+            if(!area1Condition) {
+                if(doubleCompare(triangleArea, area1) == CompType.GT) area1Condition = true;
+            }
+            if(!area2Condition) {
+                if(doubleCompare(triangleArea, area2) == CompType.LT) area2Condition = true;
+            }
+
+            if(area1Condition && area2Condition) return true;
+        }
 
         return false;
     }
